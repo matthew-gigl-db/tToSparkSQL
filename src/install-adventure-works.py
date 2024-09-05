8c6f4224250459ca5e94a9fc29ecc8e09ac67c47
matthew-gigl-db <matthew-gigl-db>
2024-09-05 18:17:49 +0000

    added the adventure work files to fixtures

--- src/install-adventure-works.py
+++ src/install-adventure-works.py
@@ -1,4 +1,22 @@
 # Databricks notebook source
-display(
-  spark.sql("select current_user()")
-)
+# MAGIC %sql
+# MAGIC
+# MAGIC DECLARE OR REPLACE VARIABLE new_catalog STRING;
+# MAGIC SET VAR new_catalog = :`bundle.catalog`;
+# MAGIC SELECT new_catalog; 
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC
+# MAGIC EXECUTE IMMEDIATE "create catalog if not exists new_catalog;" 
+
+# COMMAND ----------
+
+# MAGIC %sql 
+# MAGIC
+# MAGIC EXECUTE IMMEDIATE "create schema if not exists IDENTIFIER(new_catalog || '.adventure');"  
+
+# COMMAND ----------
+
+1+1
