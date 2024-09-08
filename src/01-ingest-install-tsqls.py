9cd47d813217db4b5841a85be399d43adcc98804
matthew-gigl-db <matthew-gigl-db>
2024-09-08 22:53:44 +0000

    markdown title chunk, notebook rename

---
+++ src/01-ingest-install-tsqls.py
@@ -0,0 +1,11 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC # Ingest T-SQL Install Files as Data
+# MAGIC
+# MAGIC The purpose of this notebook is to ingest the OLTP and DW T-SQL install files from the project's *fixtures* folder as data files that we can parse and send to an LLM later for conversion to Spark SQL.  
+# MAGIC
+# MAGIC ***
+
+# COMMAND ----------
+
+
